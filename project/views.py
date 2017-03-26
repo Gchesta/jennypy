5c8c720a6107aad99f67d5e18a19769fbb71f671
Governor <elimushwari@gmail.com>
2017-03-26 18:54:00 +0300

    Modify templates to display posts from database.

--- project/views.py
+++ project/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Presentation
+from django.utils import timezone
 
 def presentation_list(request):
-    return render(request, 'project/presentation_list.html', {})
+    presentations = Presentation.objects.all()
+    return render(request, 'project/presentation_list.html', {'presentations':presentations})
 
 # Create your views here.
